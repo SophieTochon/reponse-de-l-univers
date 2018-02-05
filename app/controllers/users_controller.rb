e6ecdbfcc9181fc73046476516c1d5af20fac2fa
« SophieTochon <sophie.tochon@gmail.com>
2018-02-05 10:25:55 +0100

    Edit and delete Ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to @user
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.delete
+  	redirect_to users_index_path
   end
 
   def index

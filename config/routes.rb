170b9ab5d1df540cd7165161f1b8fcdeaacc498c
« SophieTochon <sophie.tochon@gmail.com>
2018-02-05 12:47:09 +0100

    Login Ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
 
+  root "static_pages#home"
   get "/secret", to: "static_pages#secret"
 
   get 'users/new'
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get 'users/index'
 
   resources :users
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

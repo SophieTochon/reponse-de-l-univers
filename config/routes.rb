c41cbce523173cb755c29c823671eeefdbad328f
« SophieTochon <sophie.tochon@gmail.com>
2018-02-05 10:07:48 +0100

    Create with has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
